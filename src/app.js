e39a5b4ce90c62611dabe7f33a32c495c14f50f9
Curs279 <136276439+Curs279@users.noreply.github.com>
2023-11-05 23:14:28 +0000

    Se creó el generador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Any string");
+  document.querySelector("#boton").onclick = () => {
+    document.querySelector("#generador").innerHTML = `<h1> ${
+      resultadoFinal[Math.floor(Math.random() * resultadoFinal.length)]
+    }</h1>`;
+  };
+
+  let pronombres = ["Nuestro", "El", "Mi"];
+  let adjetivos = ["asombroso", "triste", "increíble"];
+  let sustantivos = ["momento", "curso", "trabajo"];
+  let dominios = [".com", ".es", ".gg", ".perdónporelretraso"];
+
+  let resultadoFinal = [];
+
+  for (let pronombre of pronombres) {
+    for (let adjetivo of adjetivos) {
+      for (let sustantivo of sustantivos) {
+        for (let dominio of dominios) {
+          resultadoFinal.push(`${pronombre}${adjetivo}${sustantivo}${dominio}`);
+        }
+      }
+    }
+  }
 };
